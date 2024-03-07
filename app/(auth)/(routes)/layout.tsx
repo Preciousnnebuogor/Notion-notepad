e62406fa1f03bbc55a020c144589921716ff1512
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-03-07 18:17:42 +0100

    added icons on the layout.tsx

--- app/(auth)/(routes)/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div className="h-full bg-red-500">
-    {children} 
-     </div>;
-}
--- app/(auth)/(routes)/register/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Register() {
-    return <div>
-        <p>register in page</p>
-    </div>
-}
