7e6ff7a77311dd2db3577fed069bd0facbd9c570
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-03-07 23:56:03 +0100

    added heros,header,logo,footer

---
+++ app/(marketing)/_components/heros.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+export default function Heros() {
+  return (
+    <div className="flex flex-col items-center justify-center max-w-5xl">
+      <div className="flex items-center">
+        <div
+          className="relative w-[300pxl] h-[300px] sm:w-[350px]
+     sm:h-[350px] md:h-[400px] md:w-[400px]"
+        >
+          <Image
+            src="/man light.png"
+            fill
+            className="object-contain"
+            alt="man"
+          />
+        </div>
+
+        <div className="relative h-[400px] w-[400px] hidden md:block">
+          <Image
+          src="/read light.png"
+          fill
+          className="object-contain"
+          alt="man"
+          />
+        </div>
+
+      </div>
+    </div>
+  );
+}
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,4 +1,6 @@
+import Footer from "./_components/footer";
 import Heading from "./_components/heading";
+import Heros from "./_components/heros";
 
 
 export default function MarketingPage() {
@@ -7,8 +9,9 @@ export default function MarketingPage() {
       <div className="flex flex-col items-center justify-center 
       md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
         <Heading/>
+        <Heros/>
       </div>
-      
+      <Footer/>
     </div>
   ); 
 }
