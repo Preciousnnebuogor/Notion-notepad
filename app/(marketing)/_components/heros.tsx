89de2b090cc54b4c7564609c5a8b1f1a348dcd78
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-03-11 21:21:40 +0100

    added

--- app/(marketing)/_components/heros.tsx
+++ app/(marketing)/_components/heros.tsx
@@ -4,9 +4,8 @@ export default function Heros() {
     <div className="flex flex-col items-center justify-center max-w-5xl">
       <div className="flex items-center">
         <div
-          className="relative w-[300pxl] h-[300px] sm:w-[350px]
-     sm:h-[350px] md:h-[400px] md:w-[400px]"
-        >
+          className="relative w-[300px] h-[300px] sm:w-[350px]
+     sm:h-[350px] md:h-[400px] md:w-[400px]">
           <Image
             src="/man light.png"
             fill
@@ -20,8 +19,8 @@ export default function Heros() {
           src="/read light.png"
           fill
           className="object-contain"
-          alt="man"
-          />
+          alt="read"
+          /> 
         </div>
 
       </div>
