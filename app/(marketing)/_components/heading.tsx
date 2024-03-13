13ce78a5e0d236171a14a1c0b9c12c0fd12dee86
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-03-13 22:05:38 +0100

    installed convex

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -5,7 +5,7 @@ import { ArrowRight } from "lucide-react";
 
 export default function Heading() {
   return (
-    <div className="max-w-3xl space-y-4">
+    <div className="max-w-3xl space-y-4 ">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
         Your Ideas, Document and Plan. Unified. Welcome to{" "}
         <span className="underline">Notion</span>
--- app/(marketing)/_components/heros.tsx
+++ app/(marketing)/_components/heros.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 export default function Heros() {
   return (
-    <div className="flex flex-col items-center justify-center max-w-5xl  dark:bg-[#1F1F1F]">
-      <div className="flex items-center">
+    <div className="flex flex-col items-center justify-center max-w-5xl dark:bg-[#1F1F1F]">
+      <div className="flex items-center  ">
         <div
           className="relative w-[300px] h-[300px] sm:w-[350px]
      sm:h-[350px] md:h-[400px] md:w-[400px]"
