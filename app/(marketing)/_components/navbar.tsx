9a995824e8e223a88ef29040b70097d808cf645d
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-03-17 22:01:50 +0100

    added to navbar.tsx

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -3,8 +3,13 @@ import useScrollTop from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import Logo from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+
 
 export default function Navbar() {
+  const {isAuthenticated, isLoading} = useConvexAuth();
   const scrolled = useScrollTop();
 
   return (
@@ -16,6 +21,24 @@ export default function Navbar() {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+       {isLoading && (
+        <p>Loading...</p>
+       )}
+        {!isAuthenticated && !isLoading && (
+          <>
+          <SignInButton mode="modal">
+            <Button variant="ghost" size="sm">
+              Log in
+            </Button>
+          </SignInButton>
+
+          <SignInButton mode="modal">
+            <Button size="sm">
+              Get Notion free
+            </Button>
+          </SignInButton>
+          </>
+        )}
         <ModeToggle/>
       </div>
     </div>
