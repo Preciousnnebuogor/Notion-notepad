10d3cd050203369a834d3e555265c76645a823a9
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-03-17 21:39:58 +0100

    added to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,27 +2,29 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Notion",
-  description: "Notepad app that enables you work better,faster and more convient to use",
+  description:
+    "Notepad app that enables you work better,faster and more convient to use",
 
-  icons:{
-   icon:[
+  icons: {
+    icon: [
       {
         media: "(prefers-color-scheme:light)",
         url: "/N no-background.png",
-        href: "/N no-background.png"
+        href: "/N no-background.png",
       },
       {
         media: "(prefers-color-scheme:dark)",
         url: "/N dark no-background.png",
-        href: "/N dark no-background.png"
+        href: "/N dark no-background.png",
       },
-  ]
-}
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -33,15 +35,18 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider 
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-        storageKey="notion-theme">
-        {children}
-        </ThemeProvider>
-        </body>
+        <ConvexClientProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="notion-theme"
+          >
+            {children}
+          </ThemeProvider>
+        </ConvexClientProvider>
+      </body>
     </html>
   );
 }
